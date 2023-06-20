676cfe554b8adb228df528aef81f28ca54c6d198
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-06-20 17:08:05 +0300

    veri yapiları 01 add

---
+++ veri_yapilari/01_uygulamaDegiskenler/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace _01_uygulamaDegiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte? kilo = null;
+            System.Console.WriteLine(kilo);
+
+            bool? satistami = true;
+            bool? _satistami = false;
+            System.Console.WriteLine(satistami);
+            System.Console.WriteLine(_satistami);
+
+
+
+        string ad = "ömer";
+        string soyad = "birgül";
+        string fullName = ad + " " +  soyad;
+        System.Console.WriteLine(fullName);
+        }
+    }
+}
