17d9d36ec1568e13ce88b80192c7c3fd0d6e6514
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-06-20 22:42:57 +0300

    add uygulama veri tipi donusumu

---
+++ veri_yapilari/02_uygulamaVeriTipiDonusumu/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace _02_mainMetodu;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+       Console.Write("Kısa Kenarı Giriniz: ");
+       int kisa = int.Parse(Console.ReadLine());
+
+       Console.Write("Uzun Kenarı Giriniz: ");
+       int uzun = int.Parse(Console.ReadLine());
+
+       var alan = kisa * uzun;
+       var cevre = (kisa + uzun) * 2;
+       
+       Console.WriteLine($"alan:{alan} çevre: {cevre}");
+        
+    }
+}
