a1c5ef4327a354953ebf8ddeb2668f4483c20fdd
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-06-27 23:54:08 +0300

    add arrays

---
+++ veri_yapilari/04_diziler/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _04_diziler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] isimler = {"ahmet", "çınar","ömer","faruk","duygu"};
+        int[] numaralar = {101,102,103,104,105};
+
+        System.Console.WriteLine($"Öğrenci adı: {isimler[0]} ve numara: {numaralar[0]}");
+        System.Console.WriteLine($"Öğrenci adı: {isimler[1]} ve numara: {numaralar[1]}");
+        System.Console.WriteLine($"Öğrenci adı: {isimler[2]} ve numara: {numaralar[2]}");
+        System.Console.WriteLine($"Öğrenci adı: {isimler[3]} ve numara: {numaralar[3]}");
+        System.Console.WriteLine($"Öğrenci adı: {isimler[4]} ve numara: {numaralar[4]}");
+    }
+}
