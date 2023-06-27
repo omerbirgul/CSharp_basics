e0e95a5984f559c847d23eb7b973a96fd800f6da
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-06-27 23:23:45 +0300

    add string page

---
+++ veri_yapilari/03_strings/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _03_strings;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("surname: ");
+        string surname = Console.ReadLine();
+
+        Console.Write("age: ");
+        int age = int.Parse(Console.ReadLine());
+
+
+        // string name = "Ömerrrrr";
+        // string surname= "Birgül";
+        // int age = 15;
+
+        // string str = "My name is " + name + " " + surname + " I'am  " + age + " years old";
+        string str = $"My name is {name} {surname}. I am {age} years old.";
+
+        System.Console.WriteLine(str);
+    }
+}
