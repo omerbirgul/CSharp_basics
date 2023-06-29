486efe27b08923c54f1fb744311a270f91d61edd
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-06-29 16:51:29 +0300

    operatorler uygulaması 01

---
+++ operatorler/03_uygulama2Operatorler/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+namespace _03_uygulama2Operatorler;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Girilen iki sayıdan hangisi daha büyüktür?
+        Console.Write("Ilk sayiyi gir: ");
+        int sayi1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ikinci sayiyi gir: ");
+        int sayi2 = int.Parse(Console.ReadLine());
+        
+        var sonuc = (sayi1 > sayi2);
+        System.Console.WriteLine("sayi1 büyük mü sayi2? " + sonuc);
+
+    }
+}
