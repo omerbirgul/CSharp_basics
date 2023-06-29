5fec851cd9cd98ebd5b392bdc47c6c9e0d065c96
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-06-29 16:37:42 +0300

    karsilastirma operatorleri

---
+++ operatorler/02_karsılastirmaOperatorleri/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _02_karsılastirmaOperatorleri;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int a=5,b=5,c=10;
+        string username = "omerbirgul";
+        string password = "123456";
+
+
+        string sonuc = (username == "omerbirgul")? 
+        (password == "123456")? "username doğru  parola doğru": "username doğru parola yanlış":
+        (password == "123456")? "username yanlış parola doğru": "username yanlış parola yanlış";
+
+        System.Console.WriteLine(sonuc);
+    }
+}
