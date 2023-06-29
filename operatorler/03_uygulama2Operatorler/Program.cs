20d72017c0daff4ae02accd147e04de6a53dddf0
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-06-29 20:05:30 +0300

    operator uygulama 05

--- operatorler/03_uygulama2Operatorler/Program.cs
+++ operatorler/03_uygulama2Operatorler/Program.cs
@@ -50,23 +50,48 @@ class Program
 
         // 4-) Girilen 3 sayiyi buyukluk olarak karsilastiriniz.
 
-        Console.Write("Ilk sayi girin: ");
-        var x =  int.Parse(Console.ReadLine());
+    //     Console.Write("Ilk sayi girin: ");
+    //     var x =  int.Parse(Console.ReadLine());
 
-        Console.Write("Ikinci sayi girin: ");
-        var y =  int.Parse(Console.ReadLine());
+    //     Console.Write("Ikinci sayi girin: ");
+    //     var y =  int.Parse(Console.ReadLine());
 
-        Console.Write("Ucuncu sayi girin: ");
-        var z =  int.Parse(Console.ReadLine());
+    //     Console.Write("Ucuncu sayi girin: ");
+    //     var z =  int.Parse(Console.ReadLine());
 
-       var karsilastirma = (x > y) && (x > z);
-       System.Console.WriteLine("en buyuk sayi x: " + karsilastirma);
+    //    var karsilastirma = (x > y) && (x > z);
+    //    System.Console.WriteLine("en buyuk sayi x: " + karsilastirma);
 
-       karsilastirma = (y > x) && (y > z);
-       System.Console.WriteLine("en buyuk sayi y: " + karsilastirma);
+    //    karsilastirma = (y > x) && (y > z);
+    //    System.Console.WriteLine("en buyuk sayi y: " + karsilastirma);
 
-       karsilastirma = (z > x) && (z > y);
-       System.Console.WriteLine("en buyuk sayi z: " + karsilastirma);
+    //    karsilastirma = (z > x) && (z > y);
+    //    System.Console.WriteLine("en buyuk sayi z: " + karsilastirma);
+
+
+
+    // 5-)Kullanıcıdan 2 vize (%60) ve final(%40) notunu alıp ortalama hesaplayın
+        //   Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazdırın.
+        //   Ortalama 50 olsa bilee final notu en az 30 olmalıdır.
+
+
+        Console.Write("1. Vize Notu Girin: ");
+        int vize1 = int.Parse(Console.ReadLine());
+
+        Console.Write("2. Vize Notu Girin: ");
+        int vize2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Final Notu Girin: ");
+        int final = int.Parse(Console.ReadLine());
+
+        int ortalama = (vize1 * 30 / 100) + (vize2 * 30 / 100) + (final * 40 / 100);
+        var durum = ((ortalama >= 50) && (final >= 30))? "GEÇTİN" : "KALDIN";
+        
+
+        System.Console.WriteLine( " Ortalama: " + ortalama);
+        System.Console.WriteLine("Durum: " + durum);
+
+
 
     }
 }
