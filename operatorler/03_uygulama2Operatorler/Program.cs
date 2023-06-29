3acf0059258e82aa9f3bee42ac7fec3533162065
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-06-29 17:02:00 +0300

    operator uygulaması 02

--- operatorler/03_uygulama2Operatorler/Program.cs
+++ operatorler/03_uygulama2Operatorler/Program.cs
@@ -6,15 +6,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Girilen iki sayıdan hangisi daha büyüktür?
-        Console.Write("Ilk sayiyi gir: ");
-        int sayi1 = int.Parse(Console.ReadLine());
+        // 1-) Girilen iki sayıdan hangisi daha büyüktür?
 
-        Console.Write("Ikinci sayiyi gir: ");
-        int sayi2 = int.Parse(Console.ReadLine());
+        // Console.Write("Ilk sayiyi gir: ");
+        // int sayi1 = int.Parse(Console.ReadLine());
+
+        // Console.Write("Ikinci sayiyi gir: ");
+        // int sayi2 = int.Parse(Console.ReadLine());
         
-        var sonuc = (sayi1 > sayi2);
-        System.Console.WriteLine("sayi1 büyük mü sayi2? " + sonuc);
+        // var sonuc = (sayi1 > sayi2);
+        // System.Console.WriteLine("sayi1 büyük mü sayi2? " + sonuc);
+
+
+
+        // 2-) Parola ve email bilgisini isteyip doğruluğunu kontrol ediniz.
+
+        Console.Write("email giriniz: ");
+        var userEmail = Console.ReadLine();
+
+        Console.Write("parola giriniz: ");
+        var userPassword = Console.ReadLine();
+
+        string email = "omerbirgul@hotmail.com";
+        string password = "123456";
+
+        var statu = (userEmail == email)?
+        (userPassword == password)? "email doğru, parola doğru" : "email doğru, parola yanliş":
+        (userPassword == password)? "email yanliş, parola doğru": "email yanliş parola yanliş";
+
+        System.Console.WriteLine(statu);
 
     }
 }
