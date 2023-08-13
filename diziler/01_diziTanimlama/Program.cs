ac70718bfa1002de70a637fd870c0d07a24c7848
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-13 23:59:57 +0300

    add diziler uygulama

---
+++ diziler/01_diziTanimlama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+var ogrenciler = new string[3];
+var notlar = new int[3];
+
+System.Console.Write("1. ogrenciyi girin:");
+ogrenciler[0] = Console.ReadLine() ?? "";
+System.Console.Write("1. ogrencinin notunu girin:");
+notlar[0] = int.Parse(Console.ReadLine());
+
+System.Console.Write("2. ogrenciyi girin:");
+ogrenciler[1] = Console.ReadLine() ?? "";
+System.Console.Write("2. ogrencinin notunu girin:");
+notlar[1] = int.Parse(Console.ReadLine());
+
+System.Console.Write("3. ogrenciyi girin:");
+ogrenciler[2] = Console.ReadLine() ?? "";
+System.Console.Write("3. ogrencinin notunu girin:");
+notlar[2] = int.Parse(Console.ReadLine());
+
+foreach(var ogrenci in ogrenciler){
+    System.Console.WriteLine(ogrenci);
+}
+foreach(var not in notlar){
+    System.Console.WriteLine(not);
+}
