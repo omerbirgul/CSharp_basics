c1c1d979abd98d48a5b39df182b10ce2bec6d03b
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-15 00:11:21 +0300

    c# file reading

---
+++ dosyaYonetimi/01_dosyaOkuma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+StreamReader sr = File.OpenText("deneme.txt");
+var s = "";
+
+while((s = sr.ReadLine()) != null){
+    System.Console.WriteLine(s);
+}
+
+System.Console.WriteLine("**************************************");
+System.Console.WriteLine("**************************************");
+
+string sonuc = File.ReadAllText("deneme.txt");
+System.Console.WriteLine(sonuc);
+
+System.Console.WriteLine("**************************************");
+System.Console.WriteLine("**************************************");
+
+string[] sonuc2 = File.ReadAllLines("deneme.txt");
+System.Console.WriteLine(sonuc2[0]);
+System.Console.WriteLine(sonuc2[3]);
