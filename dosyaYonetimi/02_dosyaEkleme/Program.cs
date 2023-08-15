db7e904a7b5b56d18da1ae5aaf3ca936d493eaf1
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-15 14:31:10 +0300

    file management file writing added

---
+++ dosyaYonetimi/02_dosyaEkleme/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+// using(StreamWriter sw = File.CreateText("deneme.txt")){
+//     sw.WriteLine("merhaba");
+//     sw.WriteLine("dünya");
+//     sw.WriteLine("deneme");
+// }
+
+// using(StreamWriter sw = File.AppendText("deneme.txt")){
+//     sw.WriteLine("selam");
+//     sw.WriteLine("dünya");
+//     sw.WriteLine("çikolata");
+// }
+
+File.WriteAllText("deneme.txt","btk");
+File.AppendAllText("deneme.txt","eğitim");
+
+using(StreamReader sr = File.OpenText("deneme.txt")){
+    var s = "";
+    while((s = sr.ReadLine()) != null){
+        System.Console.WriteLine(s);
+    }
+}
