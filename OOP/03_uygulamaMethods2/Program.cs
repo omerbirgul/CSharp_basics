b82af188176c138388b5be7afa5c2303e96de3e9
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-16 23:11:40 +0300

    methods uygulama2 add

---
+++ OOP/03_uygulamaMethods2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _03_uygulamaMethods2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+
+class Araba{
+    public string Marka { get; set; }
+    public string Model { get; set; }
+    public string Renk { get; set; }
+    public bool Otomatik { get; set; }
+
+    public void Start(){
+        System.Console.WriteLine("araba calisti");
+    }
+}
