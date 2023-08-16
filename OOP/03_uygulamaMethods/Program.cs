8e9e0f714f784afaef3384e0dad588d7a0411917
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-16 12:37:27 +0300

    C# metod uygulamasi added

---
+++ OOP/03_uygulamaMethods/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace _03_uygulamaMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var soru1 = new Soru(){
+        SoruMetni = "Hangisi programlama dili degildir?",
+        Secenekler = new string[4] {"C#", "Java", "HTML", "Dart"},
+        Cevap = "HTML"};
+
+        var soru2 = new Soru(){
+        SoruMetni = "Hangisi en populer programlama dilidir?",
+        Secenekler = new string[4] {"C#", "Java", "Pyhton", "Dart"},
+        Cevap = "C#"};
+
+        var soru3 = new Soru(){
+        SoruMetni = "Hangisi web programlama platformu degildir?",
+        Secenekler = new string[4] {"Asp.Net#", "Django", "Spring", "React Native"},
+        Cevap = "React Native"};
+
+
+        var sorular = new Soru[] {soru1, soru2 , soru3};
+
+        foreach(var soru in sorular){
+            System.Console.WriteLine(soru.SoruMetni);
+            foreach(var secenek in soru.Secenekler){
+                System.Console.WriteLine(secenek);
+            }
+            Console.Write(" Cevabiniz: ");
+            var cevabimiz = (Console.ReadLine()).ToLower();
+            soru.cevapKontrol(cevabimiz);
+            if(soru.cevapKontrol(cevabimiz)){
+                System.Console.WriteLine("dogru");
+            }else{
+                System.Console.WriteLine("yanlis");
+            }
+
+            System.Console.WriteLine("*****************************");
+            System.Console.WriteLine("*****************************");
+        }
+        
+        
+    }
+
+
+}
+class Soru{
+    public string SoruMetni { get; set; }
+    public string[] Secenekler { get; set; }
+    public string Cevap { get; set; }
+
+    public bool cevapKontrol(string cevap){
+        return this.Cevap.ToLower() == cevap.ToLower();
+    }
+}
