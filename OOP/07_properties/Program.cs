7f423ebbaecd5ae69844c2703d9940578fe605a6
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-08-17 22:00:13 +0300

    add C# properties

---
+++ OOP/07_properties/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _07_properties;
+
+class Product {
+    public string Name{
+        get {
+            return _name;
+        }
+        set{
+            if(!string.IsNullOrEmpty(value)){
+                _name = value;
+            }else{
+                System.Console.WriteLine("name bos gecilemez");
+            }
+        }
+    }
+    private string _name;
+
+    private double _price;
+    public double Price {
+         get {return _price;}
+         set{
+            if(value < 0){
+                _price = 0;
+            }else{
+                _price = value;
+            }
+        } 
+        }            
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Product p = new Product();
+        System.Console.WriteLine("isim giriniz: ");
+        p.Name = Console.ReadLine();
+
+        System.Console.WriteLine("fiyat giriniz: ");
+        p.Price = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("isim: " + p.Name);
+        System.Console.WriteLine("fiyat: " + p.Price);
+        
+    }
+}
