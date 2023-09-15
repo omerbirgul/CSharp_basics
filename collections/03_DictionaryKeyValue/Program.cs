4c5596f7d7be4b59fc19ab713931b9a6e4c1b055
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-16 01:50:00 +0300

    dictionary <TKey,TValue>

---
+++ collections/03_DictionaryKeyValue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+namespace _03_DictionaryKeyValue;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int,string> plakalar = new Dictionary<int, string>();
+
+        plakalar.Add(34,"istanbul");
+        plakalar.Add(45,"manisa");
+        plakalar.Add(42,"konya");
+
+
+        Dictionary<int,string> sayilar = new Dictionary<int, string>()
+        {
+            {1,"bir"},
+            {2,"iki"},
+            {3,"uc"},
+
+        };
+
+        // foreach(KeyValuePair<int,string> plaka in plakalar){
+        //     System.Console.WriteLine($"{plaka.Key} {plaka.Value}");
+        // }
+
+        for(int i = 0; i < plakalar.Count; i++){
+            System.Console.WriteLine($"{plakalar.Keys.ElementAt(i)}  {plakalar[plakalar.Keys.ElementAt(i)]}");
+        }
+
+        System.Console.WriteLine(plakalar.Contains(new KeyValuePair<int,string>(34,"istanbul")));
+    }
+}
