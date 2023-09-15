06654e8448ed779363fddd63686812131a3aadcc
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-15 20:41:49 +0300

    ArrayList

---
+++ collections/01_arrayList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+namespace _01_arrayList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList myList = new ArrayList();
+        myList.Add(10);
+        myList.Add("10");
+        myList.Add("abc");
+        myList.Add(10.5);
+        myList.Add(true);
+
+
+        ArrayList myList2 = new ArrayList() {"***",25,18.4,"false"};
+
+        myList.Insert(1,8); // verilen index deki değişkeni değiştirir.
+        myList.InsertRange(1,myList2); // verilen index den itibaren başka bir listeyi ekler.
+
+        foreach(var i in myList){
+            System.Console.WriteLine(i);
+        }
+    }
+}
