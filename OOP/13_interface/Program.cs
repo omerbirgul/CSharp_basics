ef0b6f476ebf0d401bbcc4422c167b9e8034be45
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-15 16:40:04 +0300

    interface eklleme

--- OOP/13_interface/Program.cs
+++ OOP/13_interface/Program.cs
@@ -1,47 +1,73 @@
 ﻿namespace _13_interface;
 
 
-interface IPersonel{
-    string adSoyad {get;set;}
-     string adres { get; set; }
-     string departman { get; set; }
+interface Ikisi
+{
+    string adSoyad { get; set; }
+    string adres { get; set; }
+    double maas { get; set; }
 
-     void bilgi();
 
 }
 
-class Yonetici : IPersonel
+interface IPersonel
+{
+    string departman { get; set; }
+    void bilgi();
+
+}
+
+class Yonetici : IPersonel, Ikisi
 {
     public Yonetici(string _adsoyad, string _adres, string _departman)
     {
         this.adSoyad = _adsoyad;
-        this.adres =_adres;
+        this.adres = _adres;
         this.departman = _departman;
     }
     public string adSoyad { get; set; }
     public string adres { get; set; }
+    public string departman { get; set; }
+    public double maas { get; set; }
+
+    public void bilgi()
+    {
+    }
+}
+
+class Robot : IPersonel
+{
+
+    public Robot(string _departman)
+    {
+        this.departman = _departman;
+    }
     public string departman { get; set; }
 
     public void bilgi()
     {
-        System.Console.WriteLine($"{this.adSoyad} isimli personel {this.departman} bolumunde yoneticidir.");
+        Console.WriteLine($"{this.departman} bolumunde robot.");
+        
     }
 }
 
 
-class Isci: IPersonel{
+
+class Isci : IPersonel
+{
 
     public Isci(string _adsoyad, string _adres, string _departman)
     {
         this.adSoyad = _adsoyad;
-        this.adres =_adres;
+        this.adres = _adres;
         this.departman = _departman;
     }
     public string adSoyad { get; set; }
     public string adres { get; set; }
     public string departman { get; set; }
+    public double maas { get; set; }
 
-     public void bilgi()
+    public void bilgi()
     {
         Console.WriteLine($"{this.adSoyad} isimli personel {this.departman} bolumunde iscidir.");
     }
@@ -53,11 +79,12 @@ class Program
     static void Main(string[] args)
     {
         var personeller = new IPersonel[3];
-        personeller[0] = new Yonetici("omer","Manisa","yazilim");
-        personeller[1] = new Isci("duygu","Ankara","uretim");
-        personeller[2] = new Isci("meral","Izmir","muhasebe");
+        personeller[0] = new Yonetici("omer", "Manisa", "yazilim");
+        personeller[1] = new Isci("duygu", "Ankara", "uretim");
+        personeller[2] = new Robot("muhasebe");
 
-        foreach(var i in personeller){
+        foreach (var i in personeller)
+        {
             i.bilgi();
         }
     }
