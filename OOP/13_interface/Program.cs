3a87c2805179b7aded363f88e0296819f256445a
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-15 14:00:37 +0300

    interface

---
+++ OOP/13_interface/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace _13_interface;
+
+
+interface IPersonel{
+    string adSoyad {get;set;}
+     string adres { get; set; }
+     string departman { get; set; }
+
+     void bilgi();
+
+}
+
+class Yonetici : IPersonel
+{
+    public Yonetici(string _adsoyad, string _adres, string _departman)
+    {
+        this.adSoyad = _adsoyad;
+        this.adres =_adres;
+        this.departman = _departman;
+    }
+    public string adSoyad { get; set; }
+    public string adres { get; set; }
+    public string departman { get; set; }
+
+    public void bilgi()
+    {
+        System.Console.WriteLine($"{this.adSoyad} isimli personel {this.departman} bolumunde yoneticidir.");
+    }
+}
+
+
+class Isci: IPersonel{
+
+    public Isci(string _adsoyad, string _adres, string _departman)
+    {
+        this.adSoyad = _adsoyad;
+        this.adres =_adres;
+        this.departman = _departman;
+    }
+    public string adSoyad { get; set; }
+    public string adres { get; set; }
+    public string departman { get; set; }
+
+     public void bilgi()
+    {
+        Console.WriteLine($"{this.adSoyad} isimli personel {this.departman} bolumunde iscidir.");
+    }
+
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        var personeller = new IPersonel[3];
+        personeller[0] = new Yonetici("omer","Manisa","yazilim");
+        personeller[1] = new Isci("duygu","Ankara","uretim");
+        personeller[2] = new Isci("meral","Izmir","muhasebe");
+
+        foreach(var i in personeller){
+            i.bilgi();
+        }
+    }
+}
