acb01489c9ae20f5e878571ec33dc613f9a6ed85
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-16 16:48:30 +0300

    throw exception new example

--- hataYonetimi/02_throwException/Program.cs
+++ hataYonetimi/02_throwException/Program.cs
@@ -1,33 +1,66 @@
 ﻿namespace _02_throwException;
 
+class Person
+{
+    string _name;
+    public string Name
+    {
+
+        get
+        {
+            return _name;
+        }
+        set
+        {
+            if (value.Length > 15)
+                throw new Exception("Name için en fazla 15 karakter girilebilir.");
+
+            _name = value;
+        }
+    }
+}
+
 class Program
 {
 
-    static void checkPassword(string password){
-        if(password.Length <8 || password.Length > 15)
-        throw new Exception("Parola 7-15 karakter uzunlugunda olmali.");
+    static void checkPassword(string password)
+    {
+        if (password.Length < 8 || password.Length > 15)
+            throw new Exception("Parola 7-15 karakter uzunlugunda olmali.");
 
-        if(!password.Any(char.IsDigit))
-        throw new Exception("Parola en az bir rakam icermeli.");
+        if (!password.Any(char.IsDigit))
+            throw new Exception("Parola en az bir rakam icermeli.");
 
-        if(!password.Any(char.IsLetter))
-        throw new Exception("Parola en az bir harf icermeli.");
+        if (!password.Any(char.IsLetter))
+            throw new Exception("Parola en az bir harf icermeli.");
     }
     static void Main(string[] args)
     {
 
-        System.Console.Write("Parola Girin: ");
-        string parola = Console.ReadLine();
+        // System.Console.Write("Parola Girin: ");
+        // string parola = Console.ReadLine();
+
+        // try
+        // {
+        //     checkPassword(parola);
+        //     System.Console.WriteLine("parola geçerli");
+        // }
+        // catch (Exception e)
+        // {
+        //     System.Console.WriteLine(e.Message);
+        // }
+
 
         try
         {
-            checkPassword(parola);
-            System.Console.WriteLine("parola geçerli");
+            var p = new Person();
+            p.Name = "omer                            "; 
         }
-        catch (Exception e)
+        catch (System.Exception e)
         {
             System.Console.WriteLine(e.Message);
         }
-        
+    
+
     }
 }
