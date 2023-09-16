120ad2f21142d2a4d76d4c81d7d481e0e1e5f047
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-16 17:28:51 +0300

    throw exception uygulama 2

--- hataYonetimi/03_uygulamaExceptionHandling/Program.cs
+++ hataYonetimi/03_uygulamaExceptionHandling/Program.cs
@@ -6,20 +6,48 @@ class Program
     {
         // 1-) 1,2,5a,10b,abc,10,50  liste içerisindeki sayısal değerleri bul.
 
-        var liste = new List<string>(){
-            "1","2","5a","10b","abc","10","50"
-        };
+        // var liste = new List<string>(){
+        //     "1","2","5a","10b","abc","10","50"
+        // };
 
-        foreach(var i in liste){
+        // foreach(var i in liste){
+        //     try
+        //     {
+        //         int a = int.Parse(i);
+        //         Console.WriteLine(a);
+        //     }
+        //     catch (Exception )
+        //     {
+        //         continue;
+        //     }
+        // }
+
+
+
+
+
+        // 2-) kullanıcı q değerini girmedikçe aldığınız her değerin sayısal olup olmadığını kontrol edin aksi halde hata mesaı verin
+
+        while (true)
+        {
+            System.Console.Write("Değer girin: ");
+            var deger = Console.ReadLine();
+            if (deger == "q"){
+
+                System.Console.WriteLine("çıkıldı");
+                break;
+            }
             try
             {
-                int a = int.Parse(i);
-                Console.WriteLine(a);
+                int sayi = int.Parse(deger);
+                System.Console.WriteLine($"girdiğiniz değer: {sayi}");
             }
-            catch (Exception )
+            catch (System.Exception e)
             {
+                System.Console.WriteLine("geçersiz sayı");
                 continue;
             }
+
         }
     }
 }
