8aceddf5fddaf8887bfb736f2569f50dc9090845
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-16 16:38:31 +0300

    throw exception

---
+++ hataYonetimi/02_throwException/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _02_throwException;
+
+class Program
+{
+
+    static void checkPassword(string password){
+        if(password.Length <8 || password.Length > 15)
+        throw new Exception("Parola 7-15 karakter uzunlugunda olmali.");
+
+        if(!password.Any(char.IsDigit))
+        throw new Exception("Parola en az bir rakam icermeli.");
+
+        if(!password.Any(char.IsLetter))
+        throw new Exception("Parola en az bir harf icermeli.");
+    }
+    static void Main(string[] args)
+    {
+
+        System.Console.Write("Parola Girin: ");
+        string parola = Console.ReadLine();
+
+        try
+        {
+            checkPassword(parola);
+            System.Console.WriteLine("parola geçerli");
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine(e.Message);
+        }
+        
+    }
+}
