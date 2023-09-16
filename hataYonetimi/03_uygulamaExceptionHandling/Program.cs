45228f0b6052bc126b01aeec169a84ed3dbea20d
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-16 17:18:20 +0300

    exception handling uygulama 1

---
+++ hataYonetimi/03_uygulamaExceptionHandling/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03_uygulamaExceptionHandling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 1-) 1,2,5a,10b,abc,10,50  liste içerisindeki sayısal değerleri bul.
+
+        var liste = new List<string>(){
+            "1","2","5a","10b","abc","10","50"
+        };
+
+        foreach(var i in liste){
+            try
+            {
+                int a = int.Parse(i);
+                Console.WriteLine(a);
+            }
+            catch (Exception )
+            {
+                continue;
+            }
+        }
+    }
+}
