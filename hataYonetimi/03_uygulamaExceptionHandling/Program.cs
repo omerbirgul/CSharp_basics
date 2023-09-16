c5fe309f7911cb8bee1e20588129c8a7c71fc467
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-16 17:57:28 +0300

    exception handling uygulama 3

--- hataYonetimi/03_uygulamaExceptionHandling/Program.cs
+++ hataYonetimi/03_uygulamaExceptionHandling/Program.cs
@@ -2,6 +2,8 @@
 
 class Program
 {
+
+
     static void Main(string[] args)
     {
         // 1-) 1,2,5a,10b,abc,10,50  liste içerisindeki sayısal değerleri bul.
@@ -28,26 +30,63 @@ class Program
 
         // 2-) kullanıcı q değerini girmedikçe aldığınız her değerin sayısal olup olmadığını kontrol edin aksi halde hata mesaı verin
 
-        while (true)
-        {
-            System.Console.Write("Değer girin: ");
-            var deger = Console.ReadLine();
-            if (deger == "q"){
+        // while (true)
+        // {
+        //     System.Console.Write("Değer girin: ");
+        //     var deger = Console.ReadLine();
+        //     if (deger == "q"){
 
-                System.Console.WriteLine("çıkıldı");
-                break;
-            }
+        //         System.Console.WriteLine("çıkıldı");
+        //         break;
+        //     }
+        //     try
+        //     {
+        //         int sayi = int.Parse(deger);
+        //         System.Console.WriteLine($"girdiğiniz değer: {sayi}");
+        //     }
+        //     catch (System.Exception e)
+        //     {
+        //         System.Console.WriteLine("geçersiz sayı");
+        //         continue;
+        //     }
+
+        // }
+
+
+
+
+        // 3-) Girilen parola içinde türkçe karakter araması yapınız.
+
+        Console.Write("parola: ");
+            string parola = Console.ReadLine();
+
             try
             {
-                int sayi = int.Parse(deger);
-                System.Console.WriteLine($"girdiğiniz değer: {sayi}");
+                CheckPassword(parola);
             }
-            catch (System.Exception e)
+            catch (Exception ex)
             {
-                System.Console.WriteLine("geçersiz sayı");
-                continue;
+                Console.Write(ex.Message);
             }
 
+
+
+        static void CheckPassword(string parola)
+        {
+            string turkce_karakterler = "ğ,Ğ,ç,Ç,ş,Ş,ü,Ü,ö,Ö,ı,İ";
+
+            foreach (var karakter in parola)
+            {
+                if (turkce_karakterler.IndexOf(karakter)>-1)
+                    throw new Exception("Parola türkçe karakter içeremez");
+            }
+            Console.Write("geçerli parola");
         }
+
     }
+
+
+
+
+
 }
