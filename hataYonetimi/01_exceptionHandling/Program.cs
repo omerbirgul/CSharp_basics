f4f6fe5c5b5d82e3598bba85eb22d3f6c0add3d5
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-09-16 16:17:35 +0300

    excepiton handling

---
+++ hataYonetimi/01_exceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _01_exceptionHandling;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            var sonuc = a / b;
+
+            Console.WriteLine($"{0} / {1} = {2}",a,b,sonuc);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("b sifir olamaz");
+        }
+
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
