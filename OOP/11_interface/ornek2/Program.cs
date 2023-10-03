3ad2e64881c43eb0f409681c2f263073c2c8f50a
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-03 20:08:15 +0300

    ornek2

---
+++ OOP/11_interface/ornek2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ornek2;
+
+interface Iperson
+{
+     int ID { get; set; }
+     string FirstName { get; set; }
+     string LastName { get; set; }
+}
+
+class Customer : Iperson
+{
+    public int ID { get ; set ; }
+    public string FirstName { get ; set ; }
+    public string LastName { get ; set ; }
+    public string Adress { get; set; }
+}
+
+class Student :Iperson
+{
+    public int ID { get ; set ; }
+    public string FirstName { get ; set ; }
+    public string LastName { get ; set ; }
+    public string Department { get; set; }
+    
+}
+
+class PersonManager
+{
+    public void Add(Customer customer)
+    {
+        System.Console.WriteLine(customer.FirstName);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PersonManager manager = new PersonManager();
+        manager.Add(new Customer{ID=1,FirstName="omer",LastName="birgul"});
+    }
+}
