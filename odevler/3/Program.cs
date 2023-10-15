4ba8ee6b639d7fec62eedc15ed3c7f705811fdc1
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-15 16:42:21 +0300

    patika c# odev1 soru3

---
+++ odevler/3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Pozitif bir sayi girin: ");
+        var n = int.Parse(Console.ReadLine());
+
+        List<string> kelimeListesi = new List<string>();
+        System.Console.WriteLine($"{n} adet kelime girin: ");
+        for(int i = 0; i < n; i++){
+            string kelime = Console.ReadLine();
+            kelimeListesi.Add(kelime);
+        }
+        System.Console.WriteLine("****************");
+        kelimeListesi.Reverse();
+        foreach(string item in kelimeListesi){
+            System.Console.WriteLine(item);
+        }
+    }
+}
