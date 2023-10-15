61c35fe81b366b8deddd299338dedd0c5bdd84a9
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-15 15:28:44 +0300

    patika c# odev1

---
+++ odevler/1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.Write("Pozitif bir sayi girin:" );
+        var sayi = int.Parse(Console.ReadLine());
+        List<int> sayiListesi =new List<int>();
+        System.Console.WriteLine($"{sayi} adet sayi giriniz: ");
+        for(int i =0; i <sayi; i++){
+            System.Console.Write($"Sayi #{i+1}: ");
+            var eleman = int.Parse(Console.ReadLine());
+            sayiListesi.Add(eleman);
+            
+        }
+        System.Console.WriteLine("***************************");
+        foreach(int i in sayiListesi){
+            if(i % 2 == 0){
+                System.Console.WriteLine(i);
+            }
+        }
+        
+    }
+}
