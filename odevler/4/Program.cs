e13876443d6f1def17cc12ae48ce8a0209bc37ff
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-15 17:14:31 +0300

    patika c# odev1 soru4

---
+++ odevler/4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Bir cumle yazin: ");
+        var cumle = Console.ReadLine();
+
+        var harf = harfSayisiBul(cumle);
+        var kelime = kelimeSayisiBul(cumle);
+
+
+        System.Console.WriteLine($"Cumledeki harf sayisi: {harf}");
+        System.Console.WriteLine($"Cumledeki kelime sayisi: {kelime}");
+
+    }
+
+    static int harfSayisiBul(string a){
+        string safUzunluk = a.Replace(" ","");
+        return safUzunluk.Length;
+    }
+
+    static int kelimeSayisiBul(string a){
+        string[] kelimeler = a.Split(" ");
+        return kelimeler.Length;
+
+    }
+}
