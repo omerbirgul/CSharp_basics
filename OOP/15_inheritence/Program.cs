38a5f801c5211d4eed7bdfa19aceb2c8c9e34c6d
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-10-21 18:53:52 +0300

    inheritence virtual method

---
+++ OOP/15_inheritence/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace _15_inheritence;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SqlServer sqlServer = new SqlServer();
+        sqlServer.Add();
+
+        MySql mySql = new MySql();
+        mySql.Add();
+        
+    }
+
+}
+
+class Database
+{
+    public virtual void Add()
+    {
+        System.Console.WriteLine("added by default");
+    }
+
+    public virtual void Delete()
+    {
+        System.Console.WriteLine("deleted by default");
+
+    }
+}
+
+
+class SqlServer:Database
+{
+    public override void Add()
+    {
+    System.Console.WriteLine("sql added by virtual");
+    base.Add();
+    }
+}
+
+class MySql:Database
+{
+
+
+}
