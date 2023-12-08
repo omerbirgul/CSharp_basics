d8931a5db345a5d048b930ee439910f7a55cd132
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-12-08 11:56:07 +0300

    consturcor injection added

---
+++ OOP/04_constructorInjection/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _04_constructorInjection;
+
+interface ILogger
+{
+    void Log();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        EmployeeManager employeeManager = new EmployeeManager(new FileLogger());
+        employeeManager.Add();
+    }
+}
+
+class DatabaseLogger : ILogger
+{
+    public void Log()
+    {
+        System.Console.WriteLine("Logged to Database");
+    }
+}
+
+class FileLogger : ILogger
+{
+    public void Log()
+    {
+        System.Console.WriteLine("Logged to File");
+    }
+}
+
+class EmployeeManager
+{
+    ILogger _logger;
+    public EmployeeManager(ILogger logger)
+    {
+        _logger = logger;
+    }
+    public void Add()
+    
+    {
+        _logger.Log();
+        System.Console.WriteLine("Added");
+    }
+}
