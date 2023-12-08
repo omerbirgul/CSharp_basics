2d8c9a5a52b56ecf7938b39a05dd527c5ea1ff35
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-12-08 11:10:16 +0300

    recap demo exercise added

---
+++ OOP/16_recapDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _16_recapDemo;
+
+interface ILogger
+{
+    void Log();
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.logger = new FileLogger();
+        customerManager.Add();
+    }
+}
+
+class CustomerManager
+{
+    public ILogger logger { get; set; }
+    public void Add()
+    {
+        logger.Log();
+        System.Console.WriteLine("Customer Added");
+    }
+}
+
+class DatabaseLogger:ILogger
+{
+    public void Log()
+    {
+        System.Console.WriteLine("Database Logged");
+    }
+}
+
+class FileLogger:ILogger
+{
+    public void Log()
+    {
+        System.Console.WriteLine("File Logged");
+    }
+}
