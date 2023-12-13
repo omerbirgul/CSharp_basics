b033e4ec336f4cbc438a297f1caf988cb9a034e6
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2023-12-13 11:10:06 +0300

    working with lists in ado.net

--- ADO.NET/AdoNetDemo/ProductDal.cs
+++ ADO.NET/AdoNetDemo/ProductDal.cs
@@ -10,7 +10,7 @@ namespace AdoNetDemo
 {
     public class ProductDal
     {
-        public DataTable GetAll() 
+        public List<Product> GetAll() 
         {
             SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade; integrated security=true");
             if(connection.State == ConnectionState.Closed)
@@ -19,11 +19,23 @@ namespace AdoNetDemo
             }
             SqlCommand command = new SqlCommand("Select * from Products",connection);
             SqlDataReader reader = command.ExecuteReader();
-            DataTable dataTable = new DataTable();
-            dataTable.Load(reader);
+
+            List<Product> products = new List<Product>();
+            while(reader.Read())
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = Convert.ToString(reader["Name"]),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToInt32(reader["UnitPrice"])
+                };
+                products.Add(product);
+            }
+
             reader.Close();
             connection.Close();
-            return dataTable;
+            return products;
 
         }
     }
